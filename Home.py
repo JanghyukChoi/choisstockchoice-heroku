ebff7df87411602e55888836b6864c267264f833
Janghyuk Choi <76894220+JanghyukChoi@users.noreply.github.com>
2024-04-16 12:38:49 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,5 @@
+
+
 import streamlit as st
 import requests
 import yfinance as yf
@@ -12,7 +14,9 @@ import json
 
 
 # FastAPI backend server URL
-BASE_URL = "https://fastapi-app-ozus.onrender.com"
+BASE_URL = "http://localhost:8000"
+
+
 
 def get_stock_info(country, symbol):
     """주식 정보를 가져오는 함수입니다."""
@@ -20,13 +24,20 @@ def get_stock_info(country, symbol):
     if response.status_code == 200:
         return response.json()
     else:
-        return print("함수 실행이 안됐습니다")
+        return None
+
 
 def get_stock_history(symbol, recommendation_date, current_date):
     """주식의 히스토리 데이터를 가져오는 함수입니다."""
     stock = yf.Ticker(symbol)
     hist = stock.history(start=recommendation_date, end=current_date)
     return hist
+
+
+# 데이터 캐싱을 위한 st.cache_data 데코레이터 사용
+cached_get_stock_info = st.cache_data(get_stock_info)
+cached_get_stock_history = st.cache_data(get_stock_history)
+
 
 def create_link(country, symbol):
     """주식 종목의 Yahoo Finance 페이지로의 링크를 생성합니다."""
@@ -35,37 +46,46 @@ def create_link(country, symbol):
     else:
         return f"https://finance.yahoo.com/quote/{symbol}"
 
+
 def show_stock_details(country, symbol, name):
     with st.spinner('주식 정보를 불러오는 중...'):
-        stock_info = get_stock_info(country, symbol)
+        # Ensure this function can handle async call
+        stock_info = cached_get_stock_info(country, symbol)
         if stock_info:
             st.write(f"### {name} 종목 상세 정보")
+            # Assuming `create_link` generates a clickable link to view more details
             st.write(create_link(country, symbol))
             st.write(f"**마지막 종가:** {round(stock_info['last_close'], 2)}")
-            st.write(f"**추천 날짜 종가:** {round(stock_info['recommendation_close'], 2)}")
+            st.write(
+                f"**추천 날짜 종가:** {round(stock_info['recommendation_close'], 2)}")
             st.write(f"**목표 수익률:** {stock_info['target_return']}")
             color = "green" if stock_info['return_rate'] >= 0 else "red"
             st.markdown(f"<span style='color: {color};'>**현재 수익률: {round(stock_info['return_rate'], 2)}%**</span>", unsafe_allow_html=True)
             st.markdown(f"**추천 이유:**<br> <br> {stock_info['recommendation_reason']}", unsafe_allow_html=True)
+
+            # Parse the dates from string to datetime objects
             dates = pd.to_datetime(list(stock_info['price'].keys()))
             prices = list(stock_info['price'].values())
+
             plt.figure(figsize=(10, 5))
-            plt.plot(dates, prices, label='Close Price', marker='o', linestyle='-', markersize=5)
+            plt.plot(dates, prices, label='Close Price',
+                     marker='o', linestyle='-', markersize=5)
             plt.title(f"{symbol} Closing Price Chart")
             plt.xlabel("Date")
             plt.ylabel("Close Price (USD)")
-            plt.xticks(rotation=45)
-            plt.tight_layout()
+            plt.xticks(rotation=45)  # Rotate dates for better readability
+            plt.tight_layout()  # Adjust layout to make room for the rotated date labels
             plt.legend()
             st.pyplot(plt)
         else:
             st.error("선택한 종목의 상세 정보를 가져올 수 없습니다.")
 
 
-
 def get_index_info(ticker_symbol, index_name):
     try:
-        nasdaq_data = yf.download(ticker_symbol, period="20d")
+        # 주말 및 공휴일을 고려하여 최대 7일간의 데이터를 가져옵니다.
+        nasdaq_data = yf.download(ticker_symbol, period="7d")
+        # 데이터 프레임에서 마지막으로 사용 가능한 두 개의 데이터를 추출합니다.
         closing_prices = nasdaq_data['Close'].dropna()
         if len(closing_prices) >= 2:
             latest_close = closing_prices.iloc[-1]
@@ -78,7 +98,6 @@ def get_index_info(ticker_symbol, index_name):
     except Exception as e:
         st.error(f"Error retrieving data for {index_name}: {str(e)}")
         return index_name, None, None, None
-
 
 
 
@@ -162,9 +181,9 @@ for col, (name, last_close, change, percent_change) in zip([col1, col2, col3, co
     #               change:+,.2f} ({percent_change:+,.2f}%)", delta_color="inverse")
     with col:
         if n >= 2:
-            st.metric(label=name, value=f"{last_close:,.2f}", delta=f"{change:+,.2f}",  label_visibility="collapsed")
+            st.metric(label=name, value=f"{last_close:,.2f}", delta=f"{change:+,.2f}")
         else:
-            st.metric(label=name, value=f"${last_close:,.2f}", delta=f"{change:+,.2f}",  label_visibility="collapsed")
+            st.metric(label=name, value=f"${last_close:,.2f}", delta=f"{change:+,.2f}")
         n = n + 1
 
 
@@ -284,9 +303,5 @@ with tab3:
 
 제공되는 정보는 '있는 그대로'의 정보 제공 목적으로만 사용되며, 투자의 책임 소재는 당사자에게 있음을 알려드립니다.
     """)
-
-
-
-
 
 
