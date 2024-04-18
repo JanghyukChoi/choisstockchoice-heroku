1642dbf4e34750952c724b10b03e1e8741e05db8
Janghyuk Choi <76894220+JanghyukChoi@users.noreply.github.com>
2024-04-18 11:17:50 +0900

    Update 1_Info.py

--- pages/1_Info.py
+++ pages/1_Info.py
@@ -2,20 +2,20 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
+from prophet import Prophet
+from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-START = "2015-01-01"
+START = "2020-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+stocks = ('GOOG', 'AAPL', 'MSFT', 'VZ')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
+n_years = st.slider('Months of prediction:', 1, 2)
+period = n_years * 20
 
 
 @st.cache
