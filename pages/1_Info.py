8985226e31a8322912f128e2bfa6846a6ae02d02
Janghyuk Choi <76894220+JanghyukChoi@users.noreply.github.com>
2024-04-18 10:45:40 +0900

    Update 1_Info.py

--- pages/1_Info.py
+++ pages/1_Info.py
@@ -1,34 +1,64 @@
-import yfinance as yf
 import streamlit as st
-import time
-import numpy as np
+from datetime import date
 
-st.set_page_config(page_title="KOSPI Chart Info", page_icon="📈")
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
 
-st.markdown("# KOSPI Chart Info")
-st.sidebar.header("KOSPI Chart Info")
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title('Stock Forecast App')
+
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
 
 
-st.write("""
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
 
-Shown are the stock **closing price** and ***volume*** of KOSPI!
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
 
-""")
+st.subheader('Raw data')
+st.write(data.tail())
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-# define the ticker symbol
-tickerSymbol = 'GOOGL'
-# get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-# get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
 
-st.write("""
-## Closing Price
-""")
-st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
