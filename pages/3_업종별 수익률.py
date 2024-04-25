e543586a98087befe886e3b39a04caa88b3eb49a
Janghyuk Choi <76894220+JanghyukChoi@users.noreply.github.com>
2024-04-25 18:01:52 +0900

    Update 3_업종별 수익률.py

--- pages/3_업종별 수익률.py
+++ pages/3_업종별 수익률.py
@@ -36,20 +36,40 @@ def get_sector(country):
     else:
         return None
 
+
+
 def display_sectors(country):
     sector_data = get_sector(country)
     if sector_data:
-        df = pd.DataFrame.from_dict(sector_data, orient='index')  # Convert dictionary to DataFrame
-        df = df.sort_index()  # Sort by sector names if needed
-        st.dataframe(df)  # Display the DataFrame as a table in Streamlit
+        # Convert dictionary to DataFrame and reorder columns
+        column_order = ['1 Day', '1 Week', '1 Month', '3 Months', '6 Months', '1 Year']
+        df = pd.DataFrame.from_dict(sector_data, orient='index')
+        df = df[column_order]  # Reorder columns according to the specified list
+
+        # Sort by sector names if needed
+        df = df.sort_index()
+
+        # Custom CSS to center the DataFrame and make index bold
+        css = """
+        <style>
+            div[data-testid='stBlock'] > div {margin-left: auto; margin-right: auto; width: 100%;}
+            .dataframe th {font-weight: bold;}
+            .dataframe tr th:nth-child(1) {font-weight: bold;}
+        </style>
+        """
+        st.write(css, unsafe_allow_html=True)
+        
+        st.dataframe(df, height=600)  # Adjust height if needed to display all rows
     else:
         st.write("No sector data available for the specified country.")
 
 # Streamlit UI components
 def main():
     st.title("업종별 수익률 대시보드")
-    st.write('''시기별로 각 업종의 수익률을 보여드립니다. 
-    이를 통해 업종의 **눌림목**과 **돌파시기**를 파악할 수 있습니다''')
+    st.markdown('''
+시기별로 각 업종의 수익률을 보여드립니다.  
+이를 통해 업종의 <span style="color: green;">**눌림목**</span>과 <span style="color: green;">**돌파시기**</span>를 파악할 수 있습니다
+''', unsafe_allow_html=True)
     country = st.selectbox("Choose a country", ["KR", "US"])  # Example countries
     if st.button("Show Sector Data"):
         display_sectors(country)
