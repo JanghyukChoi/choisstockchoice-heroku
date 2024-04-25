f7d6a4bb6947124416205b4387774d7d85e33233
Janghyuk Choi <76894220+JanghyukChoi@users.noreply.github.com>
2024-04-25 18:57:02 +0900

    Update 2_업종별 수익률.py

--- pages/2_업종별 수익률.py
+++ pages/2_업종별 수익률.py
@@ -77,8 +77,8 @@ def display_sectors(country):
             """, unsafe_allow_html=True)
 
         # Display the DataFrame with container width maximized
-        st.dataframe(df.style.background_gradient(), use_container_width=True, height=(len(df) + 1) * 35 + 3)
-        #st.dataframe(df.style.highlight_min(axis=0, color='salmon').highlight_max(axis=0, color="green"), use_container_width=True, height=(len(df) + 1) * 35 + 3)
+
+        st.dataframe(df.style.highlight_min(axis=0, color='salmon').highlight_max(axis=0, color="green"), use_container_width=True, height=(len(df) + 1) * 35 + 3)
     else:
 # Streamlit UI components
         st.write("No sector data available for the specified country.")
